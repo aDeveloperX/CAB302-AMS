7d5d21d286d6bbcbefe90c47abdff08fa6a0c0bd
ImBeemoX <51860496+ImBeemoX@users.noreply.github.com>
2019-06-16 03:30:14 +1000

    oop

---
+++ Pasta/Main.java
@@ -0,0 +1,14 @@
+package oop.Pasta;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// Instantiate a $12 Linguine pasta called p1
+
+		// Instantiate a $14 Spaghetti pasta called p2
+
+		// Fix the below print statements.
+		System.out.println("p1 has price: ");
+		System.out.println("p2 has type: ");
+	}
+}
---
+++ PastaModified/Pasta.java
@@ -0,0 +1,23 @@
+package oop.PastaModified;
+
+public class Pasta {
+	// Private properties
+	private int price;
+	private String type;
+
+	// Constructs a Pasta object with a price and a type
+	public Pasta(String type, int price) {
+		this.type = type;
+		this.price = price;
+	}
+
+	// Gets the price of the pasta
+	public int price() {
+		return price;
+	}
+
+	// Gets the type of the pasta
+	public String type() {
+		return type;
+	}
+}
